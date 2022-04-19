417ae94b9d724b4f8cadc1d979b1df2b0d4ed6fd
Antoine POPINEAU <antoine@popineau.eu>
2022-04-19 08:16:12 +0200

    Changed naming.

--- assets/js/components/filters.js
+++ assets/js/components/filters.js
@@ -2,23 +2,23 @@ import Vue from 'vue';
 
 Vue.filter('checkkind', (value) => {
   switch (value) {
-    case 'app_store': return 'App Store presence';
-    case 'dns': return 'DNS record';
-    case 'http': return 'HTTP request';
-    case 'ping': return 'ICMP Echo request';
-    case 'play_store': return 'Play Store presence';
-    case 'tcp': return 'TCP connection';
-    case 'tls': return 'TLS certificate expiration';
-    case 'udp': return 'UDP connection';
-    case 'whois': return 'Domain expiration';
-    default: return 'Unknown check';
+    case 'app_store': return 'App Store';
+    case 'dns': return 'DNS';
+    case 'http': return 'HTTP';
+    case 'ping': return 'Ping';
+    case 'play_store': return 'Google Play';
+    case 'tcp': return 'TCP';
+    case 'tls': return 'TLS';
+    case 'udp': return 'UDP';
+    case 'whois': return 'Domain';
+    default: return 'Unknown';
   }
 });
 
 Vue.filter('alerterkind', (value) => {
   switch (value) {
-    case 'webhook': return 'Plain webhook';
-    case 'slack': return 'Slack incoming webhook';
-    default: return 'Unknown alerter';
+    case 'webhook': return 'Webhook';
+    case 'slack': return 'Slack';
+    default: return 'Unknown';
   }
 });
