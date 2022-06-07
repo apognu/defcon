1a736760d46292375ff31f916796961b1a312725
Antoine POPINEAU <antoine@popineau.eu>
2022-06-07 17:05:55 +0200

    Added tests for the Python handler.

--- src/handlers/python.rs
+++ src/handlers/python.rs
@@ -53,3 +53,179 @@ impl<'h> Handler for PythonHandler<'h> {
     Ok(event)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::{PythonHandler, Handler};
+  use crate::{
+    config::CONTROLLER_ID,
+    model::{
+      specs::Python,
+      status::*,
+      Check
+    },
+    stash::Stash,
+  };
+
+  const SCRIPT_OK: &str = r#"
+def check():
+  return (OK, "this is the OK check message")
+  "#;
+
+  const SCRIPT_CRITICAL: &str = r#"
+def check():
+  return (CRITICAL, "this is the CRITICAL check message")
+  "#;
+
+  const SCRIPT_SYNTAX_ERROR: &str = r#"
+def check():
+invalid
+  "#;
+
+  const SCRIPT_WRONG_TYPE: &str = r#"
+def check():
+  return (None,None)
+  "#;
+
+  #[tokio::test]
+  async fn handler_python_ok() {
+    write_script("ok", SCRIPT_OK);
+
+    let handler = PythonHandler {
+      check: &Check::default(),
+      path: "/tmp".to_string(),
+    };
+
+    let spec = Python {
+      id: 0,
+      check_id: 0,
+      script: "ok".to_string()
+    };
+
+    let result = handler.run(&spec, CONTROLLER_ID, Stash::new()).await;
+    assert!(matches!(&result, Ok(_)));
+
+    let result = result.unwrap();
+    assert_eq!(result.status, OK);
+    assert_eq!(&result.message, "this is the OK check message");
+  }
+
+  #[tokio::test]
+  async fn handler_python_critical() {
+    write_script("critical", SCRIPT_CRITICAL);
+
+    let handler = PythonHandler {
+      check: &Check::default(),
+      path: "/tmp".to_string(),
+    };
+
+    let spec = Python {
+      id: 0,
+      check_id: 0,
+      script: "critical".to_string()
+    };
+
+    let result = handler.run(&spec, CONTROLLER_ID, Stash::new()).await;
+    assert!(matches!(&result, Ok(_)));
+
+    let result = result.unwrap();
+    assert_eq!(result.status, CRITICAL);
+    assert_eq!(&result.message, "this is the CRITICAL check message");
+  }
+
+  #[tokio::test]
+  async fn handler_python_missing() {
+    let handler = PythonHandler {
+      check: &Check::default(),
+      path: "/tmp".to_string(),
+    };
+
+    let spec = Python {
+      id: 0,
+      check_id: 0,
+      script: "missing".to_string()
+    };
+
+    let result = handler.run(&spec, CONTROLLER_ID, Stash::new()).await;
+    assert!(matches!(&result, Err(_)));
+
+    if let Err(err) = result {
+      assert_eq!(&err.to_string(), "No such file or directory (os error 2)");
+    }
+  }
+
+  #[tokio::test]
+  async fn handler_python_syntax_error() {
+    write_script("syntaxerror", SCRIPT_SYNTAX_ERROR);
+
+    let handler = PythonHandler {
+      check: &Check::default(),
+      path: "/tmp".to_string(),
+    };
+
+    let spec = Python {
+      id: 0,
+      check_id: 0,
+      script: "syntaxerror".to_string()
+    };
+
+    let result = handler.run(&spec, CONTROLLER_ID, Stash::new()).await;
+    assert!(matches!(&result, Err(_)));
+
+    if let Err(err) = result {
+      assert!(&err.to_string().contains("IndentationError"));
+    }
+  }
+
+  #[tokio::test]
+  async fn handler_python_no_check() {
+    write_script("nocheck", "");
+
+    let handler = PythonHandler {
+      check: &Check::default(),
+      path: "/tmp".to_string(),
+    };
+
+    let spec = Python {
+      id: 0,
+      check_id: 0,
+      script: "nocheck".to_string()
+    };
+
+    let result = handler.run(&spec, CONTROLLER_ID, Stash::new()).await;
+    assert!(matches!(&result, Err(_)));
+
+    if let Err(err) = result {
+      assert!(&err.to_string().contains("AttributeError"));
+    }
+  }
+
+  #[tokio::test]
+  async fn handler_python_wrong_type() {
+    write_script("wrongtype", SCRIPT_WRONG_TYPE);
+
+    let handler = PythonHandler {
+      check: &Check::default(),
+      path: "/tmp".to_string(),
+    };
+
+    let spec = Python {
+      id: 0,
+      check_id: 0,
+      script: "wrongtype".to_string()
+    };
+
+    let result = handler.run(&spec, CONTROLLER_ID, Stash::new()).await;
+    assert!(matches!(&result, Err(_)));
+
+    if let Err(err) = result {
+      assert!(&err.to_string().contains("TypeError"));
+    }
+  }
+
+  fn write_script(name: &str, payload: &str) {
+    use std::fs;
+    
+    fs::write(&format!("/tmp/{name}.py"), payload).unwrap();
+  }
+}
